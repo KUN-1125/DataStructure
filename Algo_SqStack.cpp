1d9717492896c05550fbfbf6fd95f4dee073392a
KUN <2728796596@qq.com>
2021-08-05 00:22:20 +0800

    master

--- Algo_SqStack.cpp
+++ Algo_SqStack.cpp
@@ -9,6 +9,12 @@ typedef struct SqStack
     int top;
 } SqStack;
 
+typedef struct BCStack
+{
+    char data[MaxSize];
+    int top;
+} BCStack;
+
 // 初始化
 void InitStack(SqStack &S)
 {
@@ -63,17 +69,62 @@ bool Stack_Print(SqStack S)
     return true;
 }
 
+// 括号匹配
+bool BracketCheck(char str[], int length)
+{
+    BCStack S;
+    S.top = -1;
+    for (int i = 0; i < length; i++)
+    {
+        if (str[i] == '(' || str[i] == '[' || str[i] == '{')
+        {
+            // Push(S,str[i]);
+            if (S.top == MaxSize - 1)
+                return false;
+            S.data[++S.top] = str[i];
+        }
+        else
+        {
+            // StackEmpty(S);
+            if (S.top == -1)
+                return false;
+            // Pop(S,topElem);
+            char topElem;
+            topElem = S.data[S.top--];
+            if (str[i] == ')' && topElem != '(')
+                return false;
+            if (str[i] == ']' && topElem != '[')
+                return false;
+            if (str[i] == '}' && topElem != '{')
+                return false;
+        }
+    }
+    if (S.top == -1)
+        return true;
+    return false;
+}
+
 int main()
 {
-    SqStack S;
-    InitStack(S);
-    Push(S, 8);
-    Push(S, 1);
-    Push(S, 9);
-    Stack_Print(S);
+    // SqStack S;
+    // InitStack(S);
+    // Push(S, 8);
+    // Push(S, 1);
+    // Push(S, 9);
+    // Stack_Print(S);
+    // int x;
+    // Pop(S, x);
+    // printf("x = %d\n", x);
+    // Stack_Print(S);
+    // return 0;
+    char str[10];
+    strcpy(str,"({}{[]}]");
     int x;
-    Pop(S, x);
-    printf("x = %d\n", x);
-    Stack_Print(S);
-    return 0;
+    bool i;
+    i=BracketCheck(str,8);
+    if(i==true)
+        x=1;
+    if(i==false)
+        x=0;
+    printf("%d",x);
 }
